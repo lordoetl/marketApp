ced361c019ef3db01f1025f327916b16bb269c07
lordoetl <lordoetl@gmail.com>
2020-02-13 15:28:01 -0800

    initialCommit

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React from "react";
+import "./App.css";
+import {Authenticator, AmplifyTheme} from 'aws-amplify-react'
+import {Auth, Hub} from 'aws-amplify'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import ProfilePage from './pages/ProfilePage'
+import MarketPage from './pages/MarketPage'
+
+
+class App extends React.Component {
+  state = {
+    user:null
+  };
+
+  componentDidMount(){
+    this.getUserData();
+    Hub.listen('auth', this, 'onHubCapsule')
+  }
+
+  getUserData=async()=>{
+    const user = await Auth.currentAuthenticatedUser();
+    user ? this.setState({user}): this.setState({user:null})
+  }
+
+  onHubCapsule=capsule=>{
+    
+    switch(capsule.payload.event){
+      case "signIn":
+        console.log('signed in')
+        this.getUserData()
+        break;
+        case "signUp":
+          console.log('signed up');
+          break;
+        case "signOut":
+          console.log("signed out")
+          this.setState({user:null})
+          break;
+        default:
+          return;
+    }
+  }
+  render() {
+    const {user}=this.state;
+    return !user?(
+    <Authenticator theme={theme}/>
+    ):
+    <Router>
+      <>
+      {/*Routes*/}
+      <div className="app-container">
+        <Route exact path="/" component = {HomePage}/>
+        <Route path="/profile" component={ProfilePage}/>
+        <Route path="/markets/:marketId" component={(
+            {match})=><MarketPage marketId={match.params.marketId}/>}
+            />
+           
+      </div>
+
+      </>
+    </Router>;
+  }
+}
+
+const theme={
+  ...AmplifyTheme,
+  navbar:{
+    ...AmplifyTheme,
+    backgroundColor: "var(--grey)"
+  },
+  button:{
+    ...AmplifyTheme.button,
+    backgroundColor:"var(--amazonOrange)"
+
+  },
+  sectionBody:{
+    ...AmplifyTheme.sectionBody,
+    padding: "5px"
+  },
+  sectionHeader:{
+    ...AmplifyTheme.sectionHeader,
+    backgroundColor: "var(--squidInk)"
+  }
+  
+
+};
+// export default withAuthenticator(App, true,[],null,theme);
+export default App;
